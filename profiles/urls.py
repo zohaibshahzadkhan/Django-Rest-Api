daa66628e1a0d43f073454ddf9c133a62ada0a71
Zohaib Shahzad <zohaibshahzad16@gmail.com>
2024-05-16 21:58:41 +0200

    Added profile detail view

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from profiles import views
 
 urlpatterns = [
-  path('profiles/', view=views.ProfileList.as_view())
+  path('profiles/', view=views.ProfileList.as_view()),
+  path('profiles/<int:pk>/', views.ProfileDetail.as_view()),
 ]
