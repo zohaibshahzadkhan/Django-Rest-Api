13796ab1f96cfa05d621e64d0b8d9c054ce0a040
Zohaib Shahzad <zohaibshahzad16@gmail.com>
2024-05-22 16:09:31 +0200

    Add Post list view and form

--- posts/serializers.py
+++ posts/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Post
 
 
-class PostSerializer(serializers.BaseSerializer):
+class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
     profile_id = serializers.ReadOnlyField(source="owner.profile.id")
     profile_image = serializers.ReadOnlyField(source="owner.profile.image.url")
@@ -12,15 +12,15 @@ class PostSerializer(serializers.BaseSerializer):
         request = self.context["request"]
         return request.user == obj.owner
 
-    def validate_image(self,value):
+    def validate_image(self, value):
         if value.size > 1024 * 1024 * 2:
-            raise serializers.ValidationError('Image size larger than 2MB!')
+            raise serializers.ValidationError("Image size larger than 2MB!")
         if value.image.width > 4096:
-            raise serializers.ValidationError('Image width larger than 4096px!')
+            raise serializers.ValidationError("Image width larger than 4096px!")
         if value.image.height > 4096:
-            raise serializers.ValidationError('Image height larger than 4096px!')
+            raise serializers.ValidationError("Image height larger than 4096px!")
         return value
-            
+
     class Meta:
         model = Post
         fields = [
