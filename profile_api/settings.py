3162e495decae383a22db9298cc3d77b97387dc7
Zohaib Shahzad <zohaibshahzad16@gmail.com>
2024-05-27 12:55:39 +0200

    Add extra fields to all apps model

--- profile_api/settings.py
+++ profile_api/settings.py
@@ -49,6 +49,8 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_filters',
+    
     'profiles',
     'rest_framework',
     'posts',
